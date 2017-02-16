dbda409d9613e33d8f0a18550a3619eccdebd7bb
okieoth <eiko.thomas@gmx.net>
2017-02-16 17:03:18 +0100

    option for switch start environment in angular

--- image/startDevserver.sh
+++ image/startDevserver.sh
@@ -2,11 +2,15 @@
 
 cd /opt/myproject
 
+if ! [ -z $RUNENV ]; then
+    STARTENV=--environment=$RUNENV
+fi
+
 if [ -f package.json ]; then
     if ! [ -d node_modules ]; then
         npm install
     fi
-    ng serve --host 0.0.0.0
+    ng serve --host 0.0.0.0 $STARTENV
 else
     echo "no project found in /opt/myproject :-/"
 fi
