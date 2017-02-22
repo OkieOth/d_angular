03fe4353082fc9e42b1da4d8d688ddf8ee4e4f5a
OkieOth <okie.othsam@gmail.com>
2017-02-22 19:34:22 +0100

    add aditional env parameter to start script

--- bin/build.sh
+++ bin/build.sh
@@ -7,9 +7,9 @@ source "$scriptPos/image_conf.sh"
 pushd "$scriptPos/.." > /dev/null
 	if docker build -t $imageName image
     then
-        echo -en "\033[1;34m  image created: $imageName \033[0m\n"
+        echo -en "\033[1;34m  Image created: $imageName \033[0m\n"
     else
-        echo -en "\033[1;31m  error while create image: $imageName \033[0m\n"
+        echo -en "\033[1;31m  Error while create image: $imageName \033[0m\n"
     fi
 popd > /dev/null
 
--- image/startDevserver.sh
+++ image/startDevserver.sh
@@ -2,15 +2,33 @@
 
 cd /opt/myproject
 
-if ! [ -z $RUNENV ]; then
-    STARTENV=--environment=$RUNENV
+if ! [ -z $ENVIRONMENT ]; then
+    echo "devserver using environment: $ENVIRONMENT"
+    STARTENV="--environment=$ENVIRONMENT"
+fi
+
+if ! [ -z $BASE_HREF ]; then
+    echo "devserver using base-href: $BASE_HREF"
+    BASEHREF="--base-href=$BASE_HREF"
+fi
+
+if ! [ -z $PROXYCONF ]; then
+    echo "devserver using proxy conf: $PROXYCONF"
+    PROXY="--proxy-conf=$PROXYCONF"
+fi
+
+if ! [ -z $PORT ]; then
+    echo "devserver using port: $PORT"
+    SERVER_PORT="--port=$PORT"
 fi
 
 if [ -f package.json ]; then
     if ! [ -d node_modules ]; then
         npm install
     fi
-    ng serve --host 0.0.0.0 $STARTENV
+    echo "start command line ..."
+    echo "ng serve --host 0.0.0.0 $STARTENV $PROXY $BASEHREF $SERVER_PORT"
+    ng serve --host 0.0.0.0 $STARTENV $PROXY $BASEHREF $SERVER_PORT
 else
     echo "no project found in /opt/myproject :-/"
 fi
