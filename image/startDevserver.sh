4961431c77bb8164a4dde0259f561b21ab7bae40
okieoth <eiko.thomas@gmx.net>
2017-02-23 09:22:21 +0100

    image don't install angular-cli, instead it uses the version from the project

--- image/startDevserver.sh
+++ image/startDevserver.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+function checkLink() {
+    if ! [ -L /usr/local/bin/$1 ]; then
+        ln -s /opt/myproject/node_modules/.bin/$1 /usr/local/bin
+    fi
+}
+
 cd /opt/myproject
+
+RUN ln -s /opt/node/bin/ng /usr/local/bin && \
+    ln -s /opt/node/bin/ngc /usr/local/bin && \
+    ln -s /opt/node/bin/tsc /usr/local/bin && \
+    ln -s /opt/node/bin/tsserver /usr/local/bin
+
 
 if ! [ -z $ENVIRONMENT ]; then
     echo "devserver using environment: $ENVIRONMENT"
@@ -26,6 +38,12 @@ if [ -f package.json ]; then
     if ! [ -d node_modules ]; then
         npm install
     fi
+    # set the links for using the application angular-cli version
+    checkLink ng
+    checkLink ngc
+    checkLink tsc
+    checkLink tsserver
+
     echo "start command line ..."
     echo "ng serve --host 0.0.0.0 $STARTENV $PROXY $BASEHREF $SERVER_PORT"
     ng serve --host 0.0.0.0 $STARTENV $PROXY $BASEHREF $SERVER_PORT
